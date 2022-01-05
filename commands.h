041c43b7074acd7bdaf4e79c2c29948fe8f3d284
Shani Shafir <shanishafir7777@gmail.com>
2022-01-05 20:57:34 +0200

    Added TrainCSVCommand

--- commands.h
+++ commands.h
@@ -12,6 +12,15 @@
 
 using namespace std;
 
+class Database {
+public:
+    string csv;
+    HybridAnomalyDetector detector;
+
+
+};
+
+
 class DefaultIO{
 public:
 	virtual string read()=0;
@@ -22,21 +31,42 @@ public:
 
 	// you may add additional methods here
 };
-
 // you may add here helper classes
-
+class StandardIO : public DefaultIO {
+public:
+    virtual void write(string text) override {
+        std::cout << text << std::endl;
+    }
+    virtual string read() override {
+        string output;
+        std::cin >> output;
+        return output;
+    }
+};
 
 // you may edit this class
 class Command{
+protected:
 	DefaultIO* dio;
+    std::string description;
+    Database *db;
 public:
-	Command(DefaultIO* dio):dio(dio){}
+	Command(DefaultIO* dio, Database* db):dio(dio), db(db){}
 	virtual void execute()=0;
 	virtual ~Command(){}
 };
 
 // implement here your command classes
-
+class TrainCSVCommand: public Command {
+public:
+    TrainCSVCommand(DefaultIO* dio, Database* db) : Command(dio, db){};
+    void execute() override {
+        dio->write("Please upload your local train CSV file.");
+        std::string fileName = dio->read();
+        TimeSeries ts = TimeSeries(fileName.c_str());
+        db->detector.learnNormal(ts);
+    }
+};
 
 
 #endif /* COMMANDS_H_ */
